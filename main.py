905c8919f35ea48970204ce5bf42a983e679c2c7
Francisco Javier Lendínez Tirado <fjlendinez@talenthackers.net>
2020-08-19 11:51:27 +0200

    Make CORS required

--- main.py
+++ main.py
@@ -18,23 +18,14 @@ apps.populate(settings.INSTALLED_APPS)
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-else:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api_router)
 
